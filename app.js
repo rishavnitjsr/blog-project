d2ce0b16105aa09b71a09ea4e90a5884761eb6f8
RISHAV KUMAR <115394387+rishavnitjsr@users.noreply.github.com>
2023-06-02 21:23:10 +0530

    "Initial Edit"

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -15,17 +13,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get('/',(req,res)=>{
+  res.render('home',{homeContent : homeStartingContent});
+});
 
+app.get('/about',(req,res)=>{
+  res.render('about',{aboutContent : aboutContent});
+});
 
-
-
-
-
-
-
-
-
-
+app.get('/contact-us',(req,res)=>{
+  res.render('contact',{contactContent : contactContent});
+});
 
 
 app.listen(3000, function() {
